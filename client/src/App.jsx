8c80c43bfad038a7f059227782221d4bc52ad770
Sriram Alavalapati <sriramalavalapatiit01@gmail.com>
2025-11-18 00:30:27 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       const apiUrl = 'http://localhost:8080/image/all';
 
       const response = await axios.get(apiUrl);
-      if (response?.status === 200) {
+      if (response?.status === 200 || response?.status === 304) {
         setImages(response?.data.data || []);
       }
     } catch (error) {
